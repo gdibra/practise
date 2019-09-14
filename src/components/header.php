adc830f2332451925c749bcefa088aa80d166be8
gdibra <g.dibra01@gmail.com>
2019-09-14 01:53:59 -0400

    init

---
+++ src/components/header.php
@@ -0,0 +1,14 @@
+<?php
+echo <<<END
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name='author' content='Gezim Dibra'>
+    <title>Practise Website</title>
+    <link rel='stylesheet' href='styles/index.css'>
+    <script type='text/javascript' src='src/js/menu.js'></script>
+</head>
+END;
+
+
+?>
---
+++ src/js/menu.js
@@ -0,0 +1,6 @@
+function menuTransparency() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("navbar").style.opacity = scrolled / 100;
+}
